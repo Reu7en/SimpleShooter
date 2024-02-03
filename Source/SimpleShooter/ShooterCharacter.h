56ed10766722cdf534c9375170c135b503002d71
Reuben Baker <baker.reuben@outlook.com>
2024-02-03 18:30:42 +0000

    Added Health Bar to HUD

--- Source/SimpleShooter/ShooterCharacter.h
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -25,6 +25,9 @@ public:
 	UFUNCTION(BlueprintPure)
 	bool IsDead() const;
 
+	UFUNCTION(BlueprintPure)
+	float GetHealthPercentage() const;
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
