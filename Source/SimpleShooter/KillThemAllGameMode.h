5796cb4995dbad5c6189f9d2011fc5a245c636d3
Reuben Baker <baker.reuben@outlook.com>
2024-02-03 16:06:22 +0000

    Setup New Game Mode

---
+++ Source/SimpleShooter/KillThemAllGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SimpleShooterGameModeBase.h"
+#include "KillThemAllGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLESHOOTER_API AKillThemAllGameMode : public ASimpleShooterGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void PawnKilled(APawn* PawnKilled) override;
+};
